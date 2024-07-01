89cfdeacbccbbcc9f72f18727d9fb054d4e116b4
Daniel Yim <daniel.yim96@gmail.com>
2024-07-01 15:39:43 -0400

    CORS

--- server/server.js
+++ server/server.js
@@ -15,7 +15,6 @@ const PORT = process.env.PORT || 5000;
 
 // favicon
 app.use(favicon(path.join(__dirname, "public", "favicon.ico")));
-
 app.use(
   cors({
     origin: [process.env.FRONTEND_URL, "http://localhost:3000"],
@@ -24,6 +23,7 @@ app.use(
     credentials: true
   })
 );
+app.options('*', cors());
 
 app.use(bodyParser.json());
 
