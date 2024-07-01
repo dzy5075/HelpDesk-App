cdfac5bd45f75686ffb090cc8cb4e15b23d27dd1
Daniel Yim <daniel.yim96@gmail.com>
2024-07-01 00:41:45 -0400

    added pg-url to database-js for deploy

--- server/config/database.js
+++ server/config/database.js
@@ -1,5 +1,6 @@
 // Cofigure Sequelize ORM to Postgres Database
 const { Sequelize } = require('sequelize');
+const { Pool } = require('pg');
 
 const database = process.env.DATABASE_NAME || 'helpdesk';
 const username = process.env.DATABASE_USER || 'helpdesk_user';
@@ -19,4 +20,18 @@ const sequelize = new Sequelize(database, username, password, {
   dialectOptions: ssl,
 });
 
+
+
+const pool = new Pool({
+  connectionString: process.env.POSTGRES_URL + "?sslmode=require",
+})
+
+// pool.connect((err) => {
+//   if (err) throw err
+//   console.log("Connect to PostgreSQL successful!")
+// })
+
+
+
+module.exports = pool
 module.exports = sequelize;
