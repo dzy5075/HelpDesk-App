0c17bbd45004a3311d8f0b27181c0a84cf37285a
Daniel Yim <daniel.yim96@gmail.com>
2024-07-01 05:18:37 -0400

    updated to connect

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,9 +1,7 @@
-// Authentication backend route
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const authController = require("../controllers/authController"); // Ensure this path is correct
+const authController = require('../controllers/authController');
 
-router.post("/register", authController.register);
-router.post("/login", authController.login);
+router.post('/login', authController.login);
 
 module.exports = router;
