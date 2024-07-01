494218316b7de321f78db44c559748f0f3b60f20
Daniel Yim <daniel.yim96@gmail.com>
2024-07-01 00:12:36 -0400

    added .env and revised database.js for vercel deployment

--- server/config/database.js
+++ server/config/database.js
@@ -1,9 +1,22 @@
 // Cofigure Sequelize ORM to Postgres Database
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('helpdesk', 'helpdesk_user', 'password', {
-  host: 'localhost',
-  dialect: 'postgres',
+const database = process.env.DATABASE_NAME || 'helpdesk';
+const username = process.env.DATABASE_USER || 'helpdesk_user';
+const password = process.env.DATABASE_PASSWORD || 'password';
+const host = process.env.DATABASE_HOST || 'localhost';
+const dialect = 'postgres';
+const ssl = process.env.NODE_ENV === 'production' ? {
+  ssl: {
+    require: true,
+    rejectUnauthorized: false
+  }
+} : {};
+
+const sequelize = new Sequelize(database, username, password, {
+  host,
+  dialect,
+  dialectOptions: ssl,
 });
 
 module.exports = sequelize;
