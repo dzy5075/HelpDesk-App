261ec733f8e84ffe36b612f2928063313d4a5de5
Daniel Yim <daniel.yim96@gmail.com>
2024-07-01 00:55:06 -0400

    revised for database-js and env for deploy

--- server/config/database.js
+++ server/config/database.js
@@ -1,11 +1,10 @@
-// Cofigure Sequelize ORM to Postgres Database
 const { Sequelize } = require('sequelize');
-const { Pool } = require('pg');
 
 const database = process.env.DATABASE_NAME || 'helpdesk';
 const username = process.env.DATABASE_USER || 'helpdesk_user';
 const password = process.env.DATABASE_PASSWORD || 'password';
 const host = process.env.DATABASE_HOST || 'localhost';
+const port = process.env.DATABASE_PORT || 5432;
 const dialect = 'postgres';
 const ssl = process.env.NODE_ENV === 'production' ? {
   ssl: {
@@ -16,22 +15,9 @@ const ssl = process.env.NODE_ENV === 'production' ? {
 
 const sequelize = new Sequelize(database, username, password, {
   host,
+  port,
   dialect,
   dialectOptions: ssl,
 });
 
-
-
-const pool = new Pool({
-  connectionString: process.env.POSTGRES_URL + "?sslmode=require",
-})
-
-// pool.connect((err) => {
-//   if (err) throw err
-//   console.log("Connect to PostgreSQL successful!")
-// })
-
-
-
-module.exports = pool
 module.exports = sequelize;
