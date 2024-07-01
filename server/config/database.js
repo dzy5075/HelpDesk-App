ff4c83fa88b9406b2414eb6c28ca0f3045718834
Daniel Yim <daniel.yim96@gmail.com>
2024-07-01 03:17:26 -0400

    added dialectModule to database-js

--- server/config/database.js
+++ server/config/database.js
@@ -1,48 +1,6 @@
-// const { Sequelize } = require('sequelize');
-
-// const database = process.env.DATABASE_NAME || 'helpdesk';
-// const username = process.env.DATABASE_USER || 'helpdesk_user';
-// const password = process.env.DATABASE_PASSWORD || 'password';
-// const host = process.env.DATABASE_HOST || 'localhost';
-// const port = process.env.DATABASE_PORT || 5432;
-// const dialect = 'postgres';
-// const dialectOptions = {
-//   ssl: {
-//     require: true,
-//     rejectUnauthorized: false
-//   }
-// };
-
-// const sequelize = new Sequelize(database, username, password, {
-//   host,
-//   port,
-//   dialect,
-//   dialectOptions
-// });
-
-// module.exports = sequelize;
-
-// const { Sequelize } = require('sequelize');
-
-// const connectionString = process.env.DATABASE_URL;
-
-// if (!connectionString) {
-//   throw new Error("DATABASE_URL environment variable is not set.");
-// }
-
-// const sequelize = new Sequelize(connectionString, {
-//   dialect: 'postgres',
-//   dialectOptions: {
-//     ssl: {
-//       require: true,
-//       rejectUnauthorized: false
-//     }
-//   }
-// });
-
-// module.exports = sequelize;
-
 const { Sequelize } = require('sequelize');
+import pg from "pg"
+
 
 const connectionString = process.env.POSTGRES_URL;
 
@@ -52,6 +10,7 @@ if (!connectionString) {
 
 const sequelize = new Sequelize(connectionString, {
   dialect: 'postgres',
+  dialectModule: pg,
   dialectOptions: {
     ssl: {
       require: true,
