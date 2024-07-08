3a4517017c3d2c24e654fc4479c7c554fa9e1fea
Daniel Yim <daniel.yim96@gmail.com>
2024-07-08 17:26:26 -0400

    fixed navbar button responsiveness for smaller screen

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 const container = document.getElementById("root");
 const root = createRoot(container);
@@ -12,3 +13,5 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+console.log('Bootstrap loaded:', typeof bootstrap !== 'undefined');
